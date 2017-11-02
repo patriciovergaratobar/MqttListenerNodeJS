56627ae1a028aafb512d040b8d95e4c21ab6b124
pvergara <patricio.vergara.tobar@gmail.com>
2017-11-02 14:20:44 -0300

    Add new script for install activemq and server start in docker

---
+++ bashUtils/initDockerActiveMQ.sh
@@ -0,0 +1,1 @@
+docker run --name='activemq' -d -e 'ACTIVEMQ_CONFIG_NAME=amqp-srv1' -e 'ACTIVEMQ_CONFIG_DEFAULTACCOUNT=false' -e 'ACTIVEMQ_ADMIN_LOGIN=admin' -e 'ACTIVEMQ_ADMIN_PASSWORD=admin' -v /data/activemq:/data -v /var/log/activemq:/var/log/activemq -p 8161:8161 -p 1883:1883 -p 5672:5672 -p 61616:61616 -p 61613:61613 webcenter/activemq
---
+++ bashUtils/installDockerActiveMQ.sh
@@ -0,0 +1,1 @@
+docker pull webcenter/activemq
